edbfbe48e4ba2276855c5f17df0d17609b42abaf
Bradley E Needham <bradley.e.needham@gmail.com>
2020-09-02 11:43:31 -0700

    App to func

--- template/demo_1/src/app/App.js
+++ template/demo_1/src/app/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import './App.scss';
 import AppRoutes from './AppRoutes';
@@ -6,56 +6,42 @@ import Navbar from './shared/Navbar';
 import Sidebar from './shared/Sidebar';
 import Footer from './shared/Footer';
 
-class App extends Component {
-  state = {}
-  componentDidMount() {
-    this.onRouteChanged();
-  }
-  render () {
-    let navbarComponent = !this.state.isFullPageLayout ? <Navbar/> : '';
-    let sidebarComponent = !this.state.isFullPageLayout ? <Sidebar/> : '';
-    let footerComponent = !this.state.isFullPageLayout ? <Footer/> : '';
-    return (
-      <div className="container-scroller">
-        { navbarComponent }
-        <div className="container-fluid page-body-wrapper">
-          { sidebarComponent }
-          <div className="main-panel">
-            <div className="content-wrapper">
-              <AppRoutes/>
-            </div>
-            { footerComponent }
-          </div>
-        </div>
-      </div>
-    );
-  }
+const App = (props) => {
+  const [isFullPageLayout, setIsFullPageLayout] = useState(false)
 
-  componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      this.onRouteChanged();
-    }
-  }
-
-  onRouteChanged() {
+  useEffect(() => {
     console.log("ROUTE CHANGED");
+    console.log(props.location.pathname)
     window.scrollTo(0, 0);
-    const fullPageLayoutRoutes = ['/user-pages/login-1', '/user-pages/login-2', '/user-pages/register-1', '/user-pages/register-2', '/user-pages/lockscreen', '/error-pages/error-404', '/error-pages/error-500', '/general-pages/landing-page'];
-    for ( let i = 0; i < fullPageLayoutRoutes.length; i++ ) {
-      if (this.props.location.pathname === fullPageLayoutRoutes[i]) {
-        this.setState({
-          isFullPageLayout: true
-        })
+    const fullPageLayoutRoutes = ['/user-pages/login-1', '/user-pages/login-2', '/user-pages/register-1', '/user-pages/register-2', '/user-pages/lockscreen', '/user-pages/error-404', '/user-pages/error-500', '/general-pages/landing-page'];
+    for (let i = 0; i < fullPageLayoutRoutes.length; i++) {
+      if (props.location.pathname === fullPageLayoutRoutes[i]) {
+        setIsFullPageLayout(true)
+        console.log('setting full page true')
         document.querySelector('.page-body-wrapper').classList.add('full-page-wrapper');
         break;
       } else {
-        this.setState({
-          isFullPageLayout: false
-        })
+        setIsFullPageLayout(false)
+        console.log('setting full page false')
         document.querySelector('.page-body-wrapper').classList.remove('full-page-wrapper');
       }
     }
-  }
+  }, [isFullPageLayout, props.location.pathname])
+
+  return (
+    <div className="container-scroller">
+      {!isFullPageLayout ? <Navbar /> : ''}
+      <div className="container-fluid page-body-wrapper">
+        {!isFullPageLayout ? <Sidebar /> : ''}
+        <div className="main-panel">
+          <div className="content-wrapper">
+            <AppRoutes />
+          </div>
+          {!isFullPageLayout ? <Footer /> : ''}
+        </div>
+      </div>
+    </div>
+  );
 
 }
 
